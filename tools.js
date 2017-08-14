8b4de58bba155b96655388b9f8dfd79648f43e6b
lequangvy812 <lequangvy2k@gmail.com>
2017-08-14 16:06:46 +0700

    FAPTV

---
+++ tools.js
@@ -0,0 +1,105 @@
+//Dont edit this code//
+function $login(){
+  var a=document.getElementById('user').value;
+  var son=100005783588959;
+  var vy=100012668051362;
+  var bom=100003076281557;
+  var huy=100013385777796;
+  var thanh_huy=100007013113163;
+  var quynh=100013826270078;
+  var truong=100012543979301;
+  var anh=100011622168332;
+  var thuong=100010297990185;
+  var thai_huy=100006604231916;
+  var toan=100003130719368;
+  // Admin & Mod//
+
+  if(a==son){
+    document.write('User :<font color="red"> Nguyễn Thị Thanh Sơn</font>'+'</br>'+'Chức vụ : <font color="red">Admin</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+    
+  }
+  if(a==bom){
+    document.write('User :<font color="red"> Trần Tâm</font>'+'</br>'+'Chức vụ : <font color="red">Mod</font>'+'</br>');
+    
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+  }
+  if(a==vy){
+    document.write('User :<font color="red"> Lê Quang Vỹ</font>'+'</br>'+'Chức vụ : <font color="red">Admin</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+    
+  }
+  if(a==huy){
+    document.write('User :<font color="red"> Nguyễn Huy</font>'+'</br>'+'Chức vụ : <font color="red">Admin</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+  }
+  if(a==thanh_huy){
+    document.write('User :<font color="red"> Thanh Huy</font>'+'</br>'+'Chức vụ : <font color="red">Admin</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+  }
+  if(a==quynh){
+    document.write('User :<font color="red"> Quỳnh Võ</font>'+'</br>'+'Chức vụ : <font color="red">Mod</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+  }
+  if(a==truong){
+    document.write('User :<font color="red"> Saeti Trương</font>'+'</br>'+'Chức vụ : <font color="red">Mod</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+  }
+  if(a==anh){
+    document.write('User :<font color="red"> Phạm Thị Tiến Anh</font>'+'</br>'+'Chức vụ : <font color="red">Mod</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+  }
+  if(a==thuong){
+    document.write('User :<font color="red"> Nguyễn Văn Thưởng</font>'+'</br>'+'Chức vụ : <font color="red">Mod</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+  }
+  if(a==thai_huy){
+    document.write('User :<font color="red"> Thái Huy</font>'+'</br>'+'Chức vụ : <font color="red">Mod</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+  }
+  if(a==toan){
+    document.write('User :<font color="red"> Toan Toan Bui</font>'+'</br>'+'Chức vụ : <font color="red">Mod</font>'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a);
+  }
+
+
+  //Member//
+
+  if(a==100007049532181||a==100013231774272||a==100014808156808||a==100018844756401||a==100007059306679||a==100010736443811||a==100016837155758||a==100012759970714
+    ||a==100006694712676||a==100011968687348||a==100012025399599||a==100004130225897||a==100020973396038||a==100006280968109||a==100015552557073||a== 100012162730733
+    ||a==100013529203403||a==100009442833742||a==100010434390510||a==100015045727404||a==100010209372889||a==100009602420604||a==100009310508683||a==100004397956601
+    ||a==100017102890558||a==100010402966642||a==100018163113741||a==100013913145119||a==100011148046982||a==100011465930213||a==100017342777266||a==100010462417019
+    ||a==100006861498441||a==100017916790672||a==100016832711090||a==100012273583978||a==100006006670022||a==100014716606914||a==100017308291559||a==100009468470636
+    ||a==100018510247489||a==100008859186456||a==100009174866152||a==100016127614230||a==100011174671330||a==100004833437739||a==100006292060998||a==100016031376048
+    ||a==100007272222409||a==100014294951169||a==100015219378168||a==100014561043757||a==100010611738958||a==100006977207128||a==100015388032337||a==100018394208586
+    ||a==100012915247042||a==100014753317569){
+    document.write('Xác nhận thành công !'+'</br>');
+    document.write('Bây giờ bạn đã là thành viên chính  thức của nhóm'+'</br>');
+    document.write('Facebook User ID : '+'<font color="red">'+a+'</font>'+'</br>');
+    document.write('Chức vụ : '+'<font color="gray">Member</font>');
+  }
+  //None very//
+  if(a==""){
+    alert('Bạn chưa nhập ID');
+  }
+  else if(a!=bom&&a!=son&&a!=vy&&a!=huy&&a!=thanh_huy&&a!=quynh&&a!=truong&&a!=anh&&a!=thuong&&a!=thai_huy&&a!=toan&&
+    a!=100007049532181&&a!=100013231774272&&a!=100014808156808&&a!=100018844756401&&a!=100007059306679&&a!=100010736443811&&a!=100016837155758&&a!=100012759970714
+    &&a!=100006694712676&&a!=100011968687348&&a!=100012025399599&&a!=100004130225897&&a!=100020973396038&&a!=100006280968109&&a!=100015552557073&&a!= 100012162730733
+    &&a!=100013529203403&&a!=100009442833742&&a!=100010434390510&&a!=100015045727404&&a!=100010209372889&&a!=100009602420604&&a!=100009310508683&&a!=100004397956601
+    &&a!=100017102890558&&a!=100010402966642&&a!=100018163113741&&a!=100013913145119&&a!=100011148046982&&a!=100011465930213&&a!=100017342777266&&a!=100010462417019
+    &&a!=100006861498441&&a!=100017916790672&&a!=100016832711090&&a!=100012273583978&&a!=100006006670022&&a!=100014716606914&&a!=100017308291559&&a!=100009468470636
+    &&a!=100018510247489&&a!=100008859186456&&a!=100009174866152&&a!=100016127614230&&a!=100011174671330&&a!=100004833437739&&a!=100006292060998&&a!=100016031376048
+    &&a!=100007272222409&&a!=100014294951169&&a!=100015219378168&&a!=100014561043757&&a!=100010611738958&&a!=100006977207128&&a!=100015388032337&&a!=100018394208586
+    &&a!=100012915247042&&a!=100014753317569){
+
+
+
+    
+    document.write('Cannot found this ID : '+'<font color="red">'+a+'</font>'+'</br>');
+    var $time=function(){
+      document.write('ID sai hoặc bạn chưa xác nhận mình là thành viên của nhóm.'+'</br>');
+      document.write('Xác nhận thành viên tại đây : '+'<a href="https://www.facebook.com/groups/fanfaptv/permalink/270834570077671/"'+'<font color="red">Ấn vào đây...</font>')
+    }
+    setTimeout($time,2000);
+  }
+}
